b0aba3987ae77423189a8de7390539a9b520eb64
liting <liting_qiu@hotmail.com>
2023-02-06 17:49:48 +0100

    add DropDownContent component

---
+++ src/components/DropDownContent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../styles/DropDownContent.css'
+
+export default function DropDownContent({ content }) {
+ 
+  return (
+        <p className="content"> { content } </p>
+  )
+}
