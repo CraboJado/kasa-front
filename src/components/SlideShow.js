c4361cee41669acfe7310e97045486061df90d48
liting <liting_qiu@hotmail.com>
2023-02-06 17:49:21 +0100

    add SlideShow component

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from "react";
+import carrousel from "../assets/images/Carrousel.png";
+import maskGroup from "../assets/images/MaskGroup.png";
+import '../styles/SlideShow.css'
 
 export default function SlideShow() {
   return (
     <section className="slideshow">
-    <div className="slides">
-        <img src="./assets/images/Carrousel.png" alt=""/>
-        <img src="./assets/images/MaskGroup.png" alt=""/>
-    </div>
-    <i className="fa-solid fa-chevron-left slideshow__chevron-left"></i>
-    <i className="fa-solid fa-chevron-right slideshow__chevron-right"></i>
-    <div className="pages">
-        <span className="current">1</span>
-        /
-        <span className="total">4</span>
-    </div>
-</section>
-  )
+      <div className="slides">
+        <img src={carrousel} alt="carrousel" />
+        <img src={maskGroup} alt="maskGroup" />
+      </div>
+      <i className="fa-solid fa-chevron-left slideshow__chevron-left"></i>
+      <i className="fa-solid fa-chevron-right slideshow__chevron-right"></i>
+      <div className="pages">
+        <span className="current">1</span>/<span className="total">4</span>
+      </div>
+    </section>
+  );
 }
