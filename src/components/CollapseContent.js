02a62d9c7262ab175927f43182a02c20ae21c9c7
liting <liting_qiu@hotmail.com>
2023-02-13 14:03:52 +0100

    add CollapseContent component

---
+++ src/components/CollapseContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/CollapseContent.css'
+
+export default function CollapseContent({ content }) {
+ 
+  return (
+        <p className="CollapseContent"> { content } </p>
+  )
+}
+
+
