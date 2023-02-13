deb67e03ffe71ac386167ef3bd07510c70c893e8
liting <liting_qiu@hotmail.com>
2023-02-13 11:29:40 +0100

    change className with BEM convention

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,11 +2,11 @@ import React from "react";
 import banner from "../assets/images/MaskGroup.png"
 import '../styles/Banner.css'
 
-export default function Banner() {
+export default function Banner(props) {
   return (
-    <section className="banner">
-      <h1>chez vous, partout et ailleurs</h1>
-      <img src={banner} alt="" />
+    <section className="Banner">
+      { props.page && <h1 className="Banner__title">chez vous, partout et ailleurs</h1>}
+      <img className="Banner__img" src={ props.banner } alt="" />
     </section>
   );
 }
