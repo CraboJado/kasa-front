a1c1cd08cc2a01362fd32fa91b2a9db31be95d50
liting <liting_qiu@hotmail.com>
2023-02-15 19:41:12 +0100

    component Carousel done

---
+++ src/components/Carousel.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import carrousel from "../assets/images/Carrousel.png";
+import maskGroup from "../assets/images/MaskGroup.png";
+import '../styles/Carousel.css'
+
+export default function Carousel({pictures}) {
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const preSlide = () => {
+    if(currentPage === 1 ) return
+    setCurrentPage(currentPage - 1)
+  }
+
+  const nextSlide = () => {
+    if(currentPage > pictures.length - 1) return
+    setCurrentPage(currentPage+1)
+  }
+
+  
+  return (
+    <section className="Carousel">
+      <div className="Carousel__img-wrap">
+        {<img  className="Carousel__img" src={pictures[currentPage-1]} alt="carrousel" />}
+      </div>
+      <i className="fa-solid fa-chevron-left Carousel__chevron-left" onClick = {preSlide}></i>
+      <i className="fa-solid fa-chevron-right Carousel__chevron-right" onClick = {nextSlide}></i>
+      <div className="Carousel__pages">
+        <span className="Carousel__current">{currentPage}</span>/<span className="Carousel__total">{pictures.length}</span>
+      </div>
+    </section>
+  );
+}
