6684a4f6b65bdea4001c97f09c8802a7b23ec162
liting <liting_qiu@hotmail.com>
2023-02-15 19:42:04 +0100

    component Collapse done

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/Collapse.css'
 
 export default function Collapse({ title, children }) {
 
+  const [isOpen, setIsOpen] = useState(true)
+
+  const showContent = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <article className="Collapse">
-      <div className="Collapse__header">
+      <div className="Collapse__header" onClick = { showContent }>
         <h3 className="Collapse__title">{title}</h3>
-        <i className="fa-sharp fa-solid fa-chevron-up Collapse__chevron-up"></i>
-        <i className="fa-sharp fa-solid fa-chevron-down Collapse__chevron-down"></i>
+        {
+          isOpen ? 
+          <i className="fa-sharp fa-solid fa-chevron-up Collapse__chevron-up"></i>
+          :
+          <i className="fa-sharp fa-solid fa-chevron-down Collapse__chevron-down"></i>
+        }
       </div>
-      {children}
+      {isOpen ? children : null}
     </article>
   );
 }
--- src/components/CollapseContent.js
+++ src/components/CollapseContent.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import '../styles/CollapseContent.css'
+import React from "react";
+import "../styles/CollapseContent.css";
 
-export default function CollapseContent({ content }) {
- 
-  return (
-        <p className="CollapseContent"> { content } </p>
-  )
+export default function CollapseContent({ content, equipments }) {
+
+  if (equipments) {
+
+    return (
+      <ul className="CollapseContent">
+        {equipments.map((el, index) => (
+          <li key={index}>{el}</li>
+        ))}
+      </ul>
+    );
+  } else {
+
+    return <p className="CollapseContent"> {content} </p>;
+    
+  }
 }
-
-
